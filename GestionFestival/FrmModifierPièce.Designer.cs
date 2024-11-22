0142fd6058059f5f60e3a70d06465ec1249f8ea4
astrop15 <myriam.gzara@gmail.com>
2024-11-22 08:49:25 +0100

    Design up

--- GestionFestival/FrmModifierPièce.Designer.cs
+++ GestionFestival/FrmModifierPièce.Designer.cs
@@ -3,15 +3,14 @@
     partial class FrmModifierPièce
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.Button btnretour;
+        private System.Windows.Forms.Button btnRetour;
+        private System.Windows.Forms.Button btnModifier;
         private System.Windows.Forms.TextBox txtNom;
         private System.Windows.Forms.TextBox txtDescription;
         private System.Windows.Forms.TextBox txtDuree;
-        private System.Windows.Forms.TextBox txtPrix;
         private System.Windows.Forms.ComboBox cmbAuteur;
         private System.Windows.Forms.ComboBox cmbTheme;
         private System.Windows.Forms.ComboBox cmbAudience;
-        private System.Windows.Forms.Button btnEnregistrer;
 
         protected override void Dispose(bool disposing)
         {
@@ -24,113 +23,65 @@
 
         private void InitializeComponent()
         {
-            this.btnretour = new System.Windows.Forms.Button();
+            this.btnRetour = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
             this.txtNom = new System.Windows.Forms.TextBox();
             this.txtDescription = new System.Windows.Forms.TextBox();
             this.txtDuree = new System.Windows.Forms.TextBox();
-            this.txtPrix = new System.Windows.Forms.TextBox();
             this.cmbAuteur = new System.Windows.Forms.ComboBox();
             this.cmbTheme = new System.Windows.Forms.ComboBox();
             this.cmbAudience = new System.Windows.Forms.ComboBox();
-            this.btnEnregistrer = new System.Windows.Forms.Button();
-            this.SuspendLayout();
 
-            // 
-            // btnretour
-            // 
-            this.btnretour.Location = new System.Drawing.Point(10, 350);
-            this.btnretour.Name = "btnretour";
-            this.btnretour.Size = new System.Drawing.Size(100, 30);
-            this.btnretour.TabIndex = 0;
-            this.btnretour.Text = "Retour";
-            this.btnretour.UseVisualStyleBackColor = true;
-            this.btnretour.Click += new System.EventHandler(this.btnretour_Click);
-
-            // 
-            // txtNom
-            // 
-            this.txtNom.Location = new System.Drawing.Point(150, 50);
-            this.txtNom.Name = "txtNom";
-            this.txtNom.Size = new System.Drawing.Size(200, 20);
-            this.txtNom.TabIndex = 1;
-
-            // 
-            // txtDescription
-            // 
-            this.txtDescription.Location = new System.Drawing.Point(150, 90);
-            this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(200, 20);
-            this.txtDescription.TabIndex = 2;
-
-            // 
-            // txtDuree
-            // 
-            this.txtDuree.Location = new System.Drawing.Point(150, 130);
-            this.txtDuree.Name = "txtDuree";
-            this.txtDuree.Size = new System.Drawing.Size(200, 20);
-            this.txtDuree.TabIndex = 3;
-
-            // 
-            // txtPrix
-            // 
-            this.txtPrix.Location = new System.Drawing.Point(150, 170);
-            this.txtPrix.Name = "txtPrix";
-            this.txtPrix.Size = new System.Drawing.Size(200, 20);
-            this.txtPrix.TabIndex = 4;
-
-            // 
-            // cmbAuteur
-            // 
-            this.cmbAuteur.FormattingEnabled = true;
-            this.cmbAuteur.Location = new System.Drawing.Point(150, 210);
-            this.cmbAuteur.Name = "cmbAuteur";
-            this.cmbAuteur.Size = new System.Drawing.Size(200, 21);
-            this.cmbAuteur.TabIndex = 5;
-
-            // 
-            // cmbTheme
-            // 
-            this.cmbTheme.FormattingEnabled = true;
-            this.cmbTheme.Location = new System.Drawing.Point(150, 250);
-            this.cmbTheme.Name = "cmbTheme";
-            this.cmbTheme.Size = new System.Drawing.Size(200, 21);
-            this.cmbTheme.TabIndex = 6;
-
-            // 
-            // cmbAudience
-            // 
-            this.cmbAudience.FormattingEnabled = true;
-            this.cmbAudience.Location = new System.Drawing.Point(150, 290);
-            this.cmbAudience.Name = "cmbAudience";
-            this.cmbAudience.Size = new System.Drawing.Size(200, 21);
-            this.cmbAudience.TabIndex = 7;
-
-            // 
-            // btnEnregistrer
-            // 
-            this.btnEnregistrer.Location = new System.Drawing.Point(150, 350);
-            this.btnEnregistrer.Name = "btnEnregistrer";
-            this.btnEnregistrer.Size = new System.Drawing.Size(100, 30);
-            this.btnEnregistrer.TabIndex = 8;
-            this.btnEnregistrer.Text = "Enregistrer";
-            this.btnEnregistrer.UseVisualStyleBackColor = true;
-            this.btnEnregistrer.Click += new System.EventHandler(this.btnEnregistrer_Click);
-
-            // 
-            // FrmModifierPièce
-            // 
-            this.ClientSize = new System.Drawing.Size(400, 400);
-            this.Controls.Add(this.btnretour);
-            this.Controls.Add(this.txtNom);
-            this.Controls.Add(this.txtDescription);
-            this.Controls.Add(this.txtDuree);
-            this.Controls.Add(this.txtPrix);
-            this.Controls.Add(this.cmbAuteur);
-            this.Controls.Add(this.cmbTheme);
-            this.Controls.Add(this.cmbAudience);
-            this.Controls.Add(this.btnEnregistrer);
+            // Form layout
+            this.ClientSize = new System.Drawing.Size(600, 400);
             this.Name = "FrmModifierPièce";
-            this.Text = "Modifier une Pièce";
+            this.Text = "Modifier une pièce de théâtre";
+
+            // Position des contrôles (alignement horizontal en ligne)
+            int xStart = 50;
+            int yStart = 50;
+            int controlWidth = 80;
+            int spacing = 120;
+
+            // Étiquettes et TextBoxes en ligne
+            string[] labels = { "Nom", "Description", "Durée", "Thème", "Audience", "Auteur" };
+            System.Windows.Forms.TextBox[] textBoxes = { txtNom, txtDescription, txtDuree };
+            System.Windows.Forms.ComboBox[] comboBoxes = { cmbTheme, cmbAudience, cmbAuteur };
+
+            for (int i = 0; i < labels.Length; i++)
+            {
+                var lbl = new System.Windows.Forms.Label();
+                lbl.Text = labels[i];
+                lbl.Location = new System.Drawing.Point(xStart + (i * spacing), yStart);
+                lbl.Size = new System.Drawing.Size(controlWidth, 20);
+                this.Controls.Add(lbl);
+
+                if (i < textBoxes.Length)
+                {
+                    textBoxes[i].Location = new System.Drawing.Point(xStart + (i * spacing), yStart + 30);
+                    textBoxes[i].Size = new System.Drawing.Size(controlWidth, 20);
+                    this.Controls.Add(textBoxes[i]);
+                }
+                else
+                {
+                    comboBoxes[i - textBoxes.Length].Location = new System.Drawing.Point(xStart + (i * spacing), yStart + 30);
+                    comboBoxes[i - textBoxes.Length].Size = new System.Drawing.Size(controlWidth, 20);
+                    this.Controls.Add(comboBoxes[i - textBoxes.Length]);
+                }
+            }
+
+            // Bouton Modifier
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.Location = new System.Drawing.Point(250, 200);
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            this.Controls.Add(this.btnModifier);
+
+            // Bouton Retour
+            this.btnRetour.Text = "Revenir en arrière";
+            this.btnRetour.Location = new System.Drawing.Point(250, 250);
+            this.btnRetour.Click += new System.EventHandler(this.btnRetour_Click);
+            this.Controls.Add(this.btnRetour);
+
             this.ResumeLayout(false);
             this.PerformLayout();
         }
--- GestionFestival/FrmModifierPièce.cs
+++ GestionFestival/FrmModifierPièce.cs
@@ -14,128 +14,70 @@ namespace GestionFestival
             InitializeComponent();
             pieceCourante = piece;
 
-            // Charger les données dans les ComboBox
+            // Charger les données dans les champs
             ChargerCombos();
-
-            if (pieceCourante != null)
-            {
-                ChargerDonnées(); // Charger les informations de la pièce dans les champs
-            }
-            else
-            {
-                MessageBox.Show("La pièce à modifier n'est pas valide.");
-            }
+            ChargerDonnées();
         }
 
         private void ChargerCombos()
         {
-            // Charger les auteurs dans cmbAuteur
-            cmbAuteur.DataSource = GestionPieces.GetPieces().GetListeAuthors();  // Récupérer les auteurs
-            cmbAuteur.DisplayMember = "Auth_name";  // Afficher le nom de l'auteur
-            cmbAuteur.ValueMember = "Auth_id";     // Utiliser l'ID de l'auteur
-            cmbAuteur.SelectedIndex = -1;          // Initialiser sans sélection
+            cmbAuteur.DataSource = GestionPieces.GetPieces().GetListeAuthors();
+            cmbAuteur.DisplayMember = "Auth_name";
+            cmbAuteur.ValueMember = "Auth_id";
+            cmbAuteur.SelectedIndex = -1;
 
-            // Charger les thèmes dans cmbTheme
-            cmbTheme.DataSource = GestionPieces.GetPieces().GetListeThemes();  // Récupérer les thèmes
-            cmbTheme.DisplayMember = "Theme_name";  // Afficher le nom du thème
-            cmbTheme.ValueMember = "Theme_id";      // Utiliser l'ID du thème
-            cmbTheme.SelectedIndex = -1;            // Initialiser sans sélection
+            cmbTheme.DataSource = GestionPieces.GetPieces().GetListeThemes();
+            cmbTheme.DisplayMember = "Theme_name";
+            cmbTheme.ValueMember = "Theme_id";
+            cmbTheme.SelectedIndex = -1;
 
-            // Charger les audiences dans cmbAudience
-            cmbAudience.DataSource = GestionPieces.GetPieces().GetListeAudiences();  // Récupérer les audiences
-            cmbAudience.DisplayMember = "Aud_categ"; // Afficher la catégorie d'audience
-            cmbAudience.ValueMember = "Aud_id";      // Utiliser l'ID de l'audience
-            cmbAudience.SelectedIndex = -1;          // Initialiser sans sélection
+            cmbAudience.DataSource = GestionPieces.GetPieces().GetListeAudiences();
+            cmbAudience.DisplayMember = "Aud_categ";
+            cmbAudience.ValueMember = "Aud_id";
+            cmbAudience.SelectedIndex = -1;
         }
 
         private void ChargerDonnées()
         {
-            // Charger les données de la pièce dans les champs de texte
             txtNom.Text = pieceCourante.Play_name;
             txtDescription.Text = pieceCourante.Play_description;
             txtDuree.Text = pieceCourante.Play_duration.ToString();
-            txtPrix.Text = pieceCourante.Play_price.ToString();
 
-            // Charger les ComboBox avec les valeurs de la pièce (sélectionner l'élément correspondant)
             if (pieceCourante.Auth != null)
-            {
-                cmbAuteur.SelectedValue = pieceCourante.Auth.Auth_id;  // Sélectionner l'auteur associé à la pièce
-            }
+                cmbAuteur.SelectedValue = pieceCourante.Auth.Auth_id;
 
             if (pieceCourante.Theme != null)
-            {
-                cmbTheme.SelectedValue = pieceCourante.Theme.Theme_id;  // Sélectionner le thème associé à la pièce
-            }
+                cmbTheme.SelectedValue = pieceCourante.Theme.Theme_id;
 
             if (pieceCourante.Aud != null)
+                cmbAudience.SelectedValue = pieceCourante.Aud.Aud_id;
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            // Mettre à jour les champs
+            pieceCourante.Play_name = txtNom.Text;
+            pieceCourante.Play_description = txtDescription.Text;
+            pieceCourante.Play_duration = int.Parse(txtDuree.Text);
+            pieceCourante.Auth.Auth_id = (int)cmbAuteur.SelectedValue;
+            pieceCourante.Theme.Theme_id = (int)cmbTheme.SelectedValue;
+            pieceCourante.Aud.Aud_id = (int)cmbAudience.SelectedValue;
+
+            try
             {
-                cmbAudience.SelectedValue = pieceCourante.Aud.Aud_id;  // Sélectionner l'audience associée à la pièce
+                GestionPieces.GetPieces().ModifierPiece(pieceCourante);
+                MessageBox.Show("Pièce modifiée avec succès !");
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur : {ex.Message}");
             }
         }
 
-        private void btnEnregistrer_Click(object sender, EventArgs e)
+        private void btnRetour_Click(object sender, EventArgs e)
         {
-            // Vérifiez si la pièce courante est initialisée
-            if (pieceCourante != null)
-            {
-                // Vérification des valeurs avant de les assigner
-                if (cmbAuteur.SelectedValue != null && cmbAuteur.SelectedIndex != -1)
-                {
-                    pieceCourante.Auth.Auth_id = (int)cmbAuteur.SelectedValue;
-                }
-                else
-                {
-                    MessageBox.Show("Veuillez sélectionner un auteur.");
-                    return;
-                }
-
-                if (cmbTheme.SelectedValue != null && cmbTheme.SelectedIndex != -1)
-                {
-                    pieceCourante.Theme.Theme_id = (int)cmbTheme.SelectedValue;
-                }
-                else
-                {
-                    MessageBox.Show("Veuillez sélectionner un thème.");
-                    return;
-                }
-
-                if (cmbAudience.SelectedValue != null && cmbAudience.SelectedIndex != -1)
-                {
-                    pieceCourante.Aud.Aud_id = (int)cmbAudience.SelectedValue;
-                }
-                else
-                {
-                    MessageBox.Show("Veuillez sélectionner une catégorie d'audience.");
-                    return;
-                }
-
-                // Mettre à jour les autres données de la pièce
-                pieceCourante.Play_name = txtNom.Text;
-                pieceCourante.Play_description = txtDescription.Text;
-                pieceCourante.Play_duration = int.Parse(txtDuree.Text);
-                pieceCourante.Play_price = double.Parse(txtPrix.Text);
-
-                try
-                {
-                    // Enregistrer les modifications de la pièce
-                    GestionPieces.GetPieces().ModifierPiece(pieceCourante);
-                    MessageBox.Show("Pièce modifiée avec succès !");
-                    this.Close();  // Fermer le formulaire après l'enregistrement
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Erreur lors de la modification : {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("La pièce courante est invalide.");
-            }
-        }
-
-        private void btnretour_Click(object sender, EventArgs e)
-        {
-            this.Close();  // Retour à la fenêtre précédente
+            this.Close();
         }
     }
 }
