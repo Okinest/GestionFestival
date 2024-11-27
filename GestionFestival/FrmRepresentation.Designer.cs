7dc20b580e1c6ea1ac49d030cb6fe00140ee845c
astrop15 <myriam.gzara@gmail.com>
2024-11-27 20:08:31 +0100

    Functional Design

--- GestionFestival/FrmRepresentation.Designer.cs
+++ GestionFestival/FrmRepresentation.Designer.cs
@@ -3,68 +3,102 @@
     partial class FrmGestionRepresentation
     {
         private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button BtnAjout;
+        private System.Windows.Forms.Button BtnModifier;
+        private System.Windows.Forms.Button BtnSupprimer;
+        private System.Windows.Forms.Button BtnRetour;
         private System.Windows.Forms.DataGridView dtgRepresentations;
-        private System.Windows.Forms.Button btnAjouter;
-        private System.Windows.Forms.Button btnSupprimer;
-        private System.Windows.Forms.TextBox txtDate;
-        private System.Windows.Forms.TextBox txtHeure;
-        private System.Windows.Forms.TextBox txtPlacesMax;
-        private System.Windows.Forms.TextBox txtLieu;
-        private System.Windows.Forms.TextBox txtPlayId;
-        private System.Windows.Forms.TextBox txtPlayName;
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHeure;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombrePlaces;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colLieu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNom;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
         private void InitializeComponent()
         {
+            this.BtnAjout = new System.Windows.Forms.Button();
+            this.BtnModifier = new System.Windows.Forms.Button();
+            this.BtnSupprimer = new System.Windows.Forms.Button();
+            this.BtnRetour = new System.Windows.Forms.Button();
             this.dtgRepresentations = new System.Windows.Forms.DataGridView();
-            this.btnAjouter = new System.Windows.Forms.Button();
-            this.btnSupprimer = new System.Windows.Forms.Button();
-            this.txtDate = new System.Windows.Forms.TextBox();
-            this.txtHeure = new System.Windows.Forms.TextBox();
-            this.txtPlacesMax = new System.Windows.Forms.TextBox();
-            this.txtLieu = new System.Windows.Forms.TextBox();
-            this.txtPlayId = new System.Windows.Forms.TextBox();
-            this.txtPlayName = new System.Windows.Forms.TextBox();
+            this.colDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHeure = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNombrePlaces = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colLieu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNom = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dtgRepresentations)).BeginInit();
             this.SuspendLayout();
+            // 
+            // BtnAjout
+            // 
+            this.BtnAjout.Location = new System.Drawing.Point(175, 337);
+            this.BtnAjout.Name = "BtnAjout";
+            this.BtnAjout.Size = new System.Drawing.Size(177, 47);
+            this.BtnAjout.TabIndex = 0;
+            this.BtnAjout.Text = "Ajouter";
+            // L'événement Click a été supprimé pour ce bouton
+            // 
+            // BtnModifier
+            // 
+            this.BtnModifier.Location = new System.Drawing.Point(433, 337);
+            this.BtnModifier.Name = "BtnModifier";
+            this.BtnModifier.Size = new System.Drawing.Size(177, 47);
+            this.BtnModifier.TabIndex = 1;
+            this.BtnModifier.Text = "Modifier";
+            // L'événement Click a été supprimé pour ce bouton
+            // 
+            // BtnSupprimer
+            // 
+            this.BtnSupprimer.Location = new System.Drawing.Point(679, 337);
+            this.BtnSupprimer.Name = "BtnSupprimer";
+            this.BtnSupprimer.Size = new System.Drawing.Size(177, 47);
+            this.BtnSupprimer.TabIndex = 2;
+            this.BtnSupprimer.Text = "Supprimer";
+            // L'événement Click a été supprimé pour ce bouton
+            // 
+            // BtnRetour
+            // 
+            this.BtnRetour.Location = new System.Drawing.Point(433, 414);
+            this.BtnRetour.Name = "BtnRetour";
+            this.BtnRetour.Size = new System.Drawing.Size(177, 47);
+            this.BtnRetour.TabIndex = 3;
+            this.BtnRetour.Text = "Revenir en arrière";
+            this.BtnRetour.Click += new System.EventHandler(this.BtnRetour_Click);
             // 
             // dtgRepresentations
             // 
             this.dtgRepresentations.BackgroundColor = System.Drawing.Color.White;
             this.dtgRepresentations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgRepresentations.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colDate,
+            this.colHeure,
+            this.colNombrePlaces,
+            this.colLieu,
+            this.colNom});
             this.dtgRepresentations.Location = new System.Drawing.Point(31, 144);
             this.dtgRepresentations.Name = "dtgRepresentations";
-            this.dtgRepresentations.RowHeadersWidth = 51;
             this.dtgRepresentations.Size = new System.Drawing.Size(991, 164);
-            this.dtgRepresentations.TabIndex = 0;
-            // 
-            // btnAjouter
-            // 
-            this.btnAjouter.Location = new System.Drawing.Point(31, 350);
-            this.btnAjouter.Name = "btnAjouter";
-            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
-            this.btnAjouter.TabIndex = 1;
-            this.btnAjouter.Text = "Ajouter";
-            // 
-            // btnSupprimer
-            // 
-            this.btnSupprimer.Location = new System.Drawing.Point(120, 350);
-            this.btnSupprimer.Name = "btnSupprimer";
-            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
-            this.btnSupprimer.TabIndex = 2;
-            this.btnSupprimer.Text = "Supprimer";
+            this.dtgRepresentations.TabIndex = 4;
             // 
             // FrmGestionRepresentation
             // 
             this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.dtgRepresentations);
-            this.Controls.Add(this.btnAjouter);
-            this.Controls.Add(this.btnSupprimer);
-            this.Controls.Add(this.txtDate);
-            this.Controls.Add(this.txtHeure);
-            this.Controls.Add(this.txtPlacesMax);
-            this.Controls.Add(this.txtLieu);
-            this.Controls.Add(this.txtPlayId);
-            this.Controls.Add(this.txtPlayName);
+            this.Controls.Add(this.BtnRetour);
+            this.Controls.Add(this.BtnSupprimer);
+            this.Controls.Add(this.BtnModifier);
+            this.Controls.Add(this.BtnAjout);
             this.Name = "FrmGestionRepresentation";
             this.Text = "Gestion des Représentations";
             this.Load += new System.EventHandler(this.FrmGestionRepresentation_Load);
--- GestionFestival/FrmRepresentation.cs
+++ GestionFestival/FrmRepresentation.cs
@@ -14,37 +14,57 @@ namespace GestionFestival
         {
             InitializeComponent();
             gestionRepresentations = new GestionRepresentations();
+            dtgRepresentations.Columns.Clear();
+            dtgRepresentations.AutoGenerateColumns = false;
+
+            // Création d'une en-tête de colonne pour la colonne Date
+            DataGridViewTextBoxColumn Date_column = new DataGridViewTextBoxColumn();
+            Date_column.DataPropertyName = "Rep_date";
+            Date_column.HeaderText = "Date";
+
+            // Création d'une en-tête de colonne pour la colonne Heure
+            DataGridViewTextBoxColumn Heure_column = new DataGridViewTextBoxColumn();
+            Heure_column.DataPropertyName = "Rep_time";
+            Heure_column.HeaderText = "Heure";
+
+            // Création d'une en-tête de colonne pour la colonne Nombre places maximum
+            DataGridViewTextBoxColumn NombrePlaces_column = new DataGridViewTextBoxColumn();
+            NombrePlaces_column.DataPropertyName = "Rep_max_seats";
+            NombrePlaces_column.HeaderText = "Nombre places maximum";
+
+            // Création d'une en-tête de colonne pour la colonne Lieu
+            DataGridViewTextBoxColumn Lieu_column = new DataGridViewTextBoxColumn();
+            Lieu_column.DataPropertyName = "Rep_lieu";
+            Lieu_column.HeaderText = "Lieu";
+
+            // Création d'une en-tête de colonne pour la colonne Nom (Pièce associée)
+            DataGridViewTextBoxColumn Nom_column = new DataGridViewTextBoxColumn();
+            Nom_column.DataPropertyName = "Play.Play_name";
+            Nom_column.HeaderText = "Nom";
+
+            // AJOUT DE CHAQUE COLONNE
+            dtgRepresentations.Columns.Add(Date_column);
+            dtgRepresentations.Columns.Add(Heure_column);
+            dtgRepresentations.Columns.Add(NombrePlaces_column);
+            dtgRepresentations.Columns.Add(Lieu_column);
+            dtgRepresentations.Columns.Add(Nom_column);
         }
 
         private void FrmGestionRepresentation_Load(object sender, EventArgs e)
         {
-            // Récupérer la liste des représentations
             List<Representation> listeRepresentations = gestionRepresentations.GetListeRepresentations();
-
-            // Vérifier si des représentations ont été récupérées
-            if (listeRepresentations.Count > 0)
-            {
-                dtgRepresentations.DataSource = listeRepresentations;
-            }
-            else
-            {
-                MessageBox.Show("Aucune représentation trouvée.");
-            }
-
-            // Rafraîchir le DataGridView pour afficher les données
-            dtgRepresentations.Refresh();
+            // Lier la liste des représentations au DataGridView
+            dtgRepresentations.DataSource = listeRepresentations;
         }
 
-        // Méthodes pour les événements des boutons (laisser les méthodes vides pour l'instant)
-        // Ces méthodes seront définies plus tard lorsque vous souhaiterez ajouter des fonctionnalités
-        private void btnAjouter_Click(object sender, EventArgs e)
+        // Retourne à FrmMenu
+        private void BtnRetour_Click(object sender, EventArgs e)
         {
-            // Fonctionnalité à ajouter plus tard
+            FrmMenu frmMenu = new FrmMenu();
+            this.Hide();
+            frmMenu.Show();
         }
 
-        private void btnSupprimer_Click(object sender, EventArgs e)
-        {
-            // Fonctionnalité à ajouter plus tard
-        }
+        
     }
 }
