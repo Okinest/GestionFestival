e4443256c3f776da474371cc63d61ef7909a6b99
Adrien - Leclère <adrien.leclere@orange.fr>
2024-12-10 12:16:34 +0100

    - Add Winforms Add reservation and Edit reservation.

---
+++ GestionFestival/FrmModifierReservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionFestival
+{
+    public partial class FrmModifierReservation : Form
+    {
+        public FrmModifierReservation()
+        {
+            InitializeComponent();
+        }
+
+        private void lblReservation_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
